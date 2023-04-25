114d6eb2abc094de5a5b7c0a299e9a37d06573af
urkezago12 <urkezagorac@gmail.com>
2023-04-25 16:43:40 -0500

    ManualHomeWork

---
+++ src/ManualHomeWork.java
@@ -0,0 +1,46 @@
+import java.sql.Driver;
+
+public class ManualHomeWork {
+    /*
+    1.Scenario-log in to Gmail
+
+    Given- user is able to launch the email on chrome broswer
+    When- user enters valid username and password
+    And- user clicks on log in button
+    Then-user is navigated to home page
+
+2.Scenario-log in to Gmail with wrong password
+
+    Given- user is able to lauch the email on chore browser
+    When-user enters valid user name
+    And-user enters wrong password
+    And-user clicks on log in button
+    Than-invalid password massage is displayed and user is not able to log in.
+
+3. Scenario-log in to Samsara driver
+
+    Given- user is able to open Samsara Driver app on chrome browser
+    When-user enters truck company and username and password
+    And- user is navigated to log in button
+    And- user clicks on log in button
+    Than- user is navigated to home page and welcome message is displayed
+
+4.Scenario-log in to Pivot Healt with valid credentials
+
+    Given- user is able to open app on bing browser
+    When-user enters username and password
+    And-user clicks to log in button
+    And-user need verification
+    And-user verification is succeful
+    Than-user us navigated to homepage and welcome messge is dusplayed
+
+5.Scenario-log in to Comed
+
+    Given-user is able to launch site on bing browser
+    When-user enters password and username
+    And-user clicks on log in button
+    And-user not exist
+    Than-user proced as a guest
+
+     */
+}
